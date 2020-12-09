6e16a36377e65b271f3bf13befed6643a7d5e35a
vuquangit <vuquangit@gmail.com>
2020-12-09 15:17:08 +0700

    Rename port listen

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: process.env.GRAPHQL_PORT || 4000 }, () =>
+app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
 
